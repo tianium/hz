1ef64637f2b28c83641b893df758d62b35f659a3
Ian Mathaiya <iannmathaiya@gmail.com>
2025-05-08 22:23:54 +0300

    update filter bg categories server action

--- src/actions/categories.ts
+++ src/actions/categories.ts
@@ -1,16 +1,28 @@
-import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
+import { defineAction } from "astro:actions";
+import { getCollection } from "astro:content";
 
 export const categories = {
     filterByCategory: defineAction({
-        accept: 'form',
         input: z.object({
-            category: z.string(),
+            category: z.string()
         }),
         handler: async ({ category }) => {
-            console.log(category);
 
-            return {success: true, category}
+            const articles = []
+
+            const allArticles = await getCollection("articles");
+            
+            if (category === "all") {
+                articles.push(...allArticles);
+                return { success: true, articles };
+            }
+
+            const filteredArticles = allArticles.filter((article) => article.data.category.toLowerCase() === category.toLowerCase());
+
+            articles.push(...filteredArticles);
+
+            return { success: true, articles};
         }
     })
 };
